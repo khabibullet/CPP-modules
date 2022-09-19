78976d20c3d93727dcd26743ed8cf6cec7b446f9
khabibullet <irek.rifson@gmail.com>
2022-09-19 10:29:43 +0300

    add toupper in ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 16:25:15 by anemesis          #+#    #+#             */
-/*   Updated: 2022/09/18 17:41:33 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/09/19 10:29:01 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ int main(int argc, char **argv) {
 	(void)argc;
 	for (int i = 1; i < argc; i++) {
 		for (int j = 0; argv[i][j] != '\0'; j++) {
-			if (argv[i][j] > 96 && argv[i][j] < 122) {
-				cout << (char)(argv[i][j] - 32);
-			} else {
-				cout << (char)(argv[i][j]);
-			}
+				cout << (char)toupper(argv[i][j]);
 		}
 	}
 	if (argc == 1) {

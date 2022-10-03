da16c1d32a0bb6870d5b163ad75cd1328c0d5848
khabibullet <irek.rifson@gmail.com>
2022-10-03 19:41:07 +0300

    fix

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,24 +6,22 @@
 /*   By: anemesis <anemesis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 16:25:15 by anemesis          #+#    #+#             */
-/*   Updated: 2022/09/19 10:29:01 by anemesis         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:00:42 by anemesis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char **argv) {
 	(void)argc;
 	for (int i = 1; i < argc; i++) {
 		for (int j = 0; argv[i][j] != '\0'; j++) {
-				cout << (char)toupper(argv[i][j]);
+				std::cout << (char)std::toupper(argv[i][j]);
 		}
 	}
 	if (argc == 1) {
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
